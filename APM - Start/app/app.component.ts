67facd88e2af6a820fd15c8d8ba03e783a62382c
Canicatti Alessandro <canale@otpos.local>
2017-06-08 17:05:51 +0200

    my edits

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-    selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+    selector: 'mc-app',
+    template: "<div><h2>{{pageTitle}}</h2></div><div><pm-products></pm-products></div><div><pm-products></pm-products></div>"
 })
-export class AppComponent { }
+export class AppComponent {
+    pageTitle: string = "My first ng2 Application"
+}
